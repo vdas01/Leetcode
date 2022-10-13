52fa9cde7d18acf86b25ea8be5899071353a9901
Vishal Das <vdas53073@gmail.com>
2022-10-13 09:51:44 +0530

    Add files via upload

---
+++ Daily Problems/October 2022/Delete_Node_In_A_LinkedList(13.10.22).cpp
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+       
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
