02028a21cc54829213ece1e6fec1cd575b97296a
Vishal Das <vdas53073@gmail.com>
2022-11-14 17:13:46 +0530

    Add files via upload

---
+++ Daily Problems/November 2022/Most_Stones_Removed_With_Same_Row_Or_Column(14.11.22).cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/description/
+class Solution {
+public:
+int dfs(vector<vector<int>>&stones,int index,vector<bool>&visited,int n){
+    visited[index] = true;
+    int result = 0;
+    for(int i=0;i<n;i++){
+        if(!visited[i] && (stones[i][0] == stones[index][0] || stones[i][1] == stones[index][1]))
+        result+= dfs(stones,i,visited,n)+1;
+    }
+    return result;
+}
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        int result = 0;
+        vector<bool>visited(n,false);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                result+=dfs(stones,i,visited,n);
+            }
+        }
+        return result;
+    }
+};
